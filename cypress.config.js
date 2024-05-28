44050ace036a3aea23565929612ae0491c056cb9
Hilal_B_P <hilal.posluoglu@hotmail.com>
2024-05-28 15:49:49 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,25 @@
 const { defineConfig } = require("cypress");
 const { registerArgosTask } = require("@argos-ci/cypress/task");
+require('dotenv').config();
 
 module.exports = defineConfig({
   defaultCommandTimeout: 30000,
   responseTimeout: 60000,
   chromeWebSecurity: false,
-  redirectionLimit: 50,
+  redirectionLimit: 100,
   userAgent: 'testing_agent',
-  env: {
-    AUTH_USER: 'staging',
-    AUTH_PASS: 'Mimfi34Voxuv',
-    js_files_url: '/js_minify/*.min.js',
-    js_files_hostname: 'www.plissee-experte.de',
-    catch_JS_Error: true,
-  },
   // setupNodeEvents can also be defined in "component"
   e2e: {
-    baseUrl: 'https://www.plissee-experte.de',
+    // baseUrl: process.env.BASE_URL,
+    baseUrl: "https://www.plissee-experte.de/",
     async setupNodeEvents(on, config) {
       registerArgosTask(on, config, {
         // Enable upload to Argos only when it runs on CI.
         uploadToArgos: !!process.env.CI,
+        mode: 'monitoring',
         // Set your Argos token (required only if you don't use GitHub Actions).
-        token: "7f5b38da220eeccf5d8c12dde30696671e59c012",
+        // token: process.env.ARGOS_TOKEN  // -->7f5b38da220eeccf5d8c12dde30696671e59c012
+        token: "7f5b38da220eeccf5d8c12dde30696671e59c012"
       });
 
       // include any other plugin code...
